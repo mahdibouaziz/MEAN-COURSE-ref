292e61868037c97d8597894ce6a08983835e1307
mahdibouaziz <mahdibouaziz@hotmail.fr>
2021-08-08 20:50:43 +0100

    added interceptor in the front to intercept every request with the token

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { AuthInterceptor } from './auth/auth.interceptor';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,13 @@ import { SignupComponent } from './auth/signup/signup.component';
     HttpClientModule,
     FormsModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -8,7 +8,12 @@ const url = 'http://localhost:3000/api/users';
   providedIn: 'root',
 })
 export class AuthService {
+  private token: string;
   constructor(private http: HttpClient) {}
+
+  getToken(): string {
+    return this.token;
+  }
 
   createUser(email: string, password: string): void {
     const authData: AuthData = { email, password };
@@ -25,8 +30,9 @@ export class AuthService {
   login(email: string, password: string) {
     const authData: AuthData = { email, password };
     this.http.post(`${url}/login`, authData).subscribe(
-      (result) => {
+      (result: any) => {
         console.log(result);
+        this.token = result.token;
       },
       (err) => {
         console.log(err);
