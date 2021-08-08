f9957906940babe2bc3e0b06328d06b95de1f0e5
mahdibouaziz <mahdibouaziz@hotmail.fr>
2021-08-08 09:54:38 +0100

    added image controls to store image and display a preview of the image

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -15,6 +15,7 @@ export class PostCreateComponent implements OnInit {
   form: FormGroup;
   post: Post;
   isLoading = false;
+  imagePreview: string;
 
   constructor(
     private postsService: PostsService,
@@ -27,6 +28,7 @@ export class PostCreateComponent implements OnInit {
         validators: [Validators.required, Validators.minLength(3)],
       }),
       content: new FormControl(null, { validators: [Validators.required] }),
+      image: new FormControl(null, { validators: [Validators.required] }),
     });
     this.route.paramMap.subscribe(
       (paramMap: ParamMap) => {
@@ -76,5 +78,16 @@ export class PostCreateComponent implements OnInit {
       );
     }
     this.form.reset();
+  }
+
+  onImagePicked(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({ image: file });
+    this.form.get('image').updateValueAndValidity();
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result as string;
+    };
+    reader.readAsDataURL(file);
   }
 }
