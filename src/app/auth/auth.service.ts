d05eb7c8d47ba5df75898030b05a0518336e6861
mahdibouaziz <mahdibouaziz@hotmail.fr>
2021-08-08 19:06:04 +0100

    linked the fron with back in signup

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AuthData } from '../models/auth-data.model';
+
+const url = 'http://localhost:3000/api/users';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  createUser(email: string, password: string): void {
+    const authData: AuthData = { email, password };
+    this.http.post(`${url}/signup`, authData).subscribe(
+      (result) => {
+        console.log(result);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+}
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -9,11 +10,14 @@ import { NgForm } from '@angular/forms';
 export class SignupComponent implements OnInit {
   isLoading = false;
 
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {}
 
   onSignup(form: NgForm) {
-    console.log(form);
+    if (form.invalid) {
+      return;
+    }
+    this.authService.createUser(form.value.email, form.value.password);
   }
 }
