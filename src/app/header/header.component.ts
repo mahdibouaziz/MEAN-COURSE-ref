76f939032735d6a823e01b16467861e574fe62c9
mahdibouaziz <mahdibouaziz@hotmail.fr>
2021-08-09 22:47:56 +0100

    saving token in localstorage and implement logic UI

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -14,6 +14,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
   constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
+    this.isAuthenticated = this.authService.getIsAuth();
     this.authListenerSub = this.authService.getAuthStatusListener().subscribe(
       (result) => {
         this.isAuthenticated = result;
